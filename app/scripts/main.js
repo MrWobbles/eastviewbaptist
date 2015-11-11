1ff68d103996f92cb0546a639ef34aab41b3d16e
Sean <mrwobbles.86@gmail.com>
2015-11-11 16:21:46 -0600

    Site Setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,25 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$('.mobile-nav-toggle' ).on( 'click', function() {
+  $(this).toggleClass( 'active' );
+  $('nav').toggleClass('active');
+});
+
+$(document).ready(function(){
+    $('#calendar-view').fullCalendar({
+    	height: 450,
+    	left:   'title',
+    	center: false,
+    	right:  false,
+    	googleCalendarApiKey: 'AIzaSyCBxIozpJqFt_ZFiVrXJoAlS2-0djnN3_k',
+        events: {
+            googleCalendarId: 'l6qg606uc1koc41i3c68kii64c@group.calendar.google.com'
+        }
+    })
+
+    $('.tab').click(function(){
+        var tab = $(this).data('tab');
+        $('.tab-container .text').removeClass('active');
+        $('.tab-container .' + tab).addClass('active');
+    });
+});
